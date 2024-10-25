4f9e2463c61a8f5ae2d30c76b712955661364812
Christiankeviny <christiankeviny007@gmail.com>
2024-10-25 21:12:54 +0700

    push

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import os
 
 filepath1= os.path.join(os.path.dirname(__file__), 'dl_monthly_aqi.csv')
