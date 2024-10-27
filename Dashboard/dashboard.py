22ff700dc6d520e0f44012bd8cdf296e24efec9d
Christiankeviny <christiankeviny007@gmail.com>
2024-10-27 21:53:35 +0700

    revisiisisisisisisi

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,17 +1,140 @@
+# code end to end ngubah all_data_chinadf.csv yg udah bersih jd bagian kotanya sampe nampilin di streamlit lokal data, komparasi grafik
+# dan kesimpulan dari hasil data dan komparasi grafik
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-filepath1= os.path.join(os.path.dirname(__file__), 'dl_monthly_aqi.csv')
-df1= pd.read_csv(filepath1)
-filepath2= os.path.join(os.path.dirname(__file__), 'dl_yearly_aqi.csv')
-df2= pd.read_csv(filepath2)
-filepath3= os.path.join(os.path.dirname(__file__), 'gy_monthly_aqi.csv')
-df3= pd.read_csv(filepath3)
-filepath4= os.path.join(os.path.dirname(__file__), 'gy_yearly_aqi.csv')
-df4= pd.read_csv(filepath4)
+filepath5= os.path.join(os.path.dirname(__file__), 'all_data_chinadf.csv')
+df= pd.read_csv(filepath5)
 
+#Bikin range values untuk SUBINDEX AQI china dari paling baik ke paling buruk
+pollutant_breakpoints = {
+    'PM2.5': [(0, 35), (35, 75), (75, 115), (115, 150), (150, 250), (250, 350), (350, 500)],
+    'PM10': [(0, 50), (50, 150), (150, 250), (250, 350), (350, 420), (420, 500), (500, 600)],
+    'SO2': [(0, 50), (50, 150), (150, 475), (475, 800), (800, 1600), (1600, 2100), (2100, 2620)],
+    'NO2': [(0, 40), (40, 80), (80, 180), (180, 280), (280, 565), (565, 750), (750, 940)],
+    'CO': [(0, 2), (2, 4), (4, 14), (14, 24), (24, 36), (36, 48), (48, 60)],
+    'O3': [(0, 100), (100, 160), (160, 215), (215, 265), (265, 800)]
+}
+
+#Bikin range values untuk INDEX AQI China dari paling baik ke paling buruk
+aqi_breakpoints = [(0, 50), (51, 100), (101, 150), (151, 200), (201, 300), (301, 400), (401, 500)]
+
+# Bikin function penentu kategori berdasarkan range
+def classify_aqi_category(aqi):
+    if aqi <= 50:
+        return 'Excellent'
+    elif 51 <= aqi <= 100:
+        return 'Good'
+    elif 101 <= aqi <= 150:
+        return 'Lightly Polluted'
+    elif 151 <= aqi <= 200:
+        return 'Moderately Polluted'
+    elif 201 <= aqi <= 300:
+        return 'Heavily Polluted'
+    elif 301 <= aqi <= 400:
+        return 'Severely Polluted'
+    elif 401 <= aqi <= 500:
+        return 'Extremely Polluted'
+    else:
+        return 'Invalid AQI'
+
+# Bikin function untuk rumus AQI
+def calculate_subindex(concentration, breakpoints):
+    for i, (c_low, c_high) in enumerate(breakpoints):
+        if c_low <= concentration <= c_high:
+            aqi_low, aqi_high = aqi_breakpoints[i]
+            return aqi_low + (aqi_high - aqi_low) * (concentration - c_low) / (c_high - c_low)
+    return None
+
+# Bikin variable untuk function berdasarkan rownya trus return nilai max sub-index sebagai nilai AQI
+def calculate_aqi_for_row(row):
+    pollutant_concentrations = {
+        'PM2.5': row['PM2.5'],
+        'PM10': row['PM10'],
+        'SO2': row['SO2'],
+        'NO2': row['NO2'],
+        'CO': row['CO'],
+        'O3': row['O3']
+    }
+
+    sub_indexes = {}
+    for pollutant, concentration in pollutant_concentrations.items():
+        if pollutant in pollutant_breakpoints and pd.notnull(concentration):
+            subindex = calculate_subindex(concentration, pollutant_breakpoints[pollutant])
+            if subindex is not None:
+                sub_indexes[pollutant] = subindex
+
+    if sub_indexes:
+        return max(sub_indexes.values())
+    return None
+
+def calculate_aqi_by_station(df):
+    # Bikin Split data berdasarkan station/kota
+    Dingling_df = df[df['station'] == 'Dingling'].copy()
+    Guanyuan_df = df[df['station'] == 'Guanyuan'].copy()
+    return Dingling_df, Guanyuan_df
+
+def create_dingling_yearly_and_monthly_df(Dingling_df):
+    # Gabungin tahun, bulan, hari, jam menjadi satu array variable datetime
+    Dingling_df['datetime'] = pd.to_datetime(Dingling_df[['year', 'month', 'day', 'hour']])
+
+    # Apply perhitungan AQI untuk setiap row di DataFrame (Dingling_df)
+    Dingling_df['AQI'] = Dingling_df.apply(calculate_aqi_for_row, axis=1)
+
+    # Extract tahun dan bulan dari datetime
+    Dingling_df['year'] = Dingling_df['datetime'].dt.year
+    Dingling_df['month'] = Dingling_df['datetime'].dt.month
+
+    # Menghitung rata-rata AQI tiap tahun
+    Dingling_yearly_aqi = Dingling_df.groupby('year')['AQI'].mean().reset_index()
+    Dingling_yearly_aqi['AQI'] = Dingling_yearly_aqi['AQI'].round(0).astype(int) # Membulatkan dan mengubah float menjadi integer
+    Dingling_yearly_aqi['AQI_Category'] = Dingling_yearly_aqi['AQI'].apply(classify_aqi_category) # Apply kategori AQI ke rata-rata AQI tahunan
+    df2= Dingling_yearly_aqi
+
+    # Menghitung rata-rata AQI tiap bulan
+    Dingling_monthly_aqi = Dingling_df.groupby('month')['AQI'].mean().reset_index() # Membulatkan dan mengubah float menjadi integer
+    Dingling_monthly_aqi['AQI'] = Dingling_monthly_aqi['AQI'].round(0).astype(int) # Apply kategori AQI ke rata-rata AQI bulanan
+    Dingling_monthly_aqi['AQI_Category'] = Dingling_monthly_aqi['AQI'].apply(classify_aqi_category)
+    df1=Dingling_monthly_aqi
+    # Mengeluarkan pivot tabel rata-rata AQI dan kategori AQI pertahun dan perbulan
+    return df2, df1
+
+def create_guanyuan_yearly_and_monthly_df(Guanyuan_df):
+    # Gabungin tahun, bulan, hari, jam menjadi satu array variable datetime
+    Guanyuan_df['datetime'] = pd.to_datetime(Guanyuan_df[['year', 'month', 'day', 'hour']])
+
+    # Apply perhitungan AQI untuk setiap row di DataFrame (Guanyuan_df)
+    Guanyuan_df['AQI'] = Guanyuan_df.apply(calculate_aqi_for_row, axis=1)
+
+    # Extract tahun dan bulan dari datetime
+    Guanyuan_df['year'] = Guanyuan_df['datetime'].dt.year
+    Guanyuan_df['month'] = Guanyuan_df['datetime'].dt.month
+
+    # Menghitung rata-rata AQI tiap tahun
+    Guanyuan_yearly_aqi = Guanyuan_df.groupby('year')['AQI'].mean().reset_index()
+    Guanyuan_yearly_aqi['AQI'] = Guanyuan_yearly_aqi['AQI'].round(0).astype(int) # Membulatkan dan mengubah float menjadi integer
+    Guanyuan_yearly_aqi['AQI_Category'] = Guanyuan_yearly_aqi['AQI'].apply(classify_aqi_category) # Apply kategori AQI ke rata-rata AQI tahunan
+    df4 = Guanyuan_yearly_aqi
+
+    # Menghitung rata-rata AQI tiap bulan
+    Guanyuan_monthly_aqi = Guanyuan_df.groupby('month')['AQI'].mean().reset_index() # Membulatkan dan mengubah float menjadi integer
+    Guanyuan_monthly_aqi['AQI'] = Guanyuan_monthly_aqi['AQI'].round(0).astype(int) # Apply kategori AQI ke rata-rata AQI bulanan
+    Guanyuan_monthly_aqi['AQI_Category'] = Guanyuan_monthly_aqi['AQI'].apply(classify_aqi_category)
+    df3 = Guanyuan_monthly_aqi
+    # Mengeluarkan pivot tabel rata-rata AQI dan kategori AQI pertahun dan perbulan
+    return df3, df4
+
+# call back Dingling_df dan Guanyuan_df  
+Dingling_df, Guanyuan_df = calculate_aqi_by_station(df)
+
+# call back dataframe tahunan and bulanan untuk setiap station/city
+df2, df1 = create_dingling_yearly_and_monthly_df(Dingling_df)
+df3, df4 = create_guanyuan_yearly_and_monthly_df(Guanyuan_df)
+
+# kode buat streamlit nampilin dataframe, komparasi grafik dan kesimpulan secara lokal pake streamlit run dashboard.py di terminal
 st.title('Tugas Streamlit Cloud and lokal')
 tab1, tab2, tab3 = st.tabs(["Dingling", "Guanyuan", "Compare Graph"])
  
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ﻿# StreamlitBangkit
+code end to end ngubah all_data_chinadf.csv yg udah bersih jd bagian kotanya sampe nampilin di streamlit lokal data, komparasi grafik
+dan kesimpulan dari hasil data dan komparasi grafik
 
+
+## install Requirements
+pip install -r requirements.txt
+
+## Run steamlit app
+```
+streamlit run dashboard.py
+```
+Local only
